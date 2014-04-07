8e1a5d49c8185fa25312777a1e13c3e93301dbc1
diego.torres.fuerte@gmail.com <diego.torres.fuerte@gmail.com@b1c4bdfa-f90d-7993-2cb7-8566f13d3ea9>
2014-04-07 04:25:27 +0000

    Initial version.

---
+++ trunk/VentasSMS/VentasSMS/AdminPaqDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VentasSMS.AdminPaq.dto
+{
+    public class TableNames {
+        public const string EMPRESAS = "MGW00001";
+    }
+    public class IndexNames {
+        public const string EMPRESAS_PK = "PRIMARYKEY";
+    }
+    public class Empresa
+    {
+        // FIELD INDEX IN TABLE
+        public const int ID_EMPRESA = 1;
+        public const int NOMBRE_EMPRESA = 2;
+        public const int RUTA_EMPRESA = 3;
+
+        // FIELD PROPERTIES FOR OBJECT
+        private long id;
+        private string nombre;
+        private string ruta;
+        public long Id { get { return id; } set { id = value; } }
+        public string Nombre { get { return nombre; } set { nombre = value; } }
+        public string Ruta { get { return ruta; } set { ruta = value; } }
+    }
+}
---
+++ trunk/VentasSMS/VentasSMS/frmMain.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Configuration;
+using System.IO;
+
+namespace VentasSMS
+{
+    public partial class frmMain : Form
+    {
+        private AdminPaqImpl api;
+        public AdminPaqImpl API { get { return api; } }
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            api = new AdminPaqImpl();
+            api.InitializeSDK();
+            this.WindowState = FormWindowState.Minimized;
+            this.Hide();
+        }
+
+        private void openConfigForm(string fileName) 
+        {
+            frmConfig fConfig = new frmConfig();
+            fConfig.API = api;
+            fConfig.textBoxFileName.Text = fileName;
+            fConfig.Show();
+        }
+
+        private void telefonosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // EVALUATE IF CONFIGURED FILE EXISTS
+            // For read access you do not need to call OpenExeConfiguraton
+            string configFilePath = ConfigurationManager.AppSettings[CommonConstants.SMS_FILE_NAME];
+            string myDocs = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            configFilePath = configFilePath.Replace("~", myDocs);
+
+            if ("".Equals(configFilePath.Trim()) || !File.Exists(configFilePath))
+            {
+                openConfigForm(configFilePath);
+            }
+            else 
+            {
+                System.Diagnostics.Process.Start(configFilePath);
+            }
+
+        }
+    }
+}
